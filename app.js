15435c4c555018ec8d9eef18b1682f1f29e0df5e
yandens <dedensetyawan17120104@gmail.com>
2022-11-17 22:31:38 +0800

    repair port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const bodyParser = require('body-parser')
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
